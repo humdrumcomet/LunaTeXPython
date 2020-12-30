4d47eaf46956acd99aa18d4a8dcdf377d4e73742
Aaron English <humdrumcomet@protonmail.com>
2020-12-30 16:12:58 -0500

    final restructuring and fixing patch file to modify both cmakelist files in lunatic-python and lunatic-python/src to use the locally built lua. setup script runs successfully and generates the .so files and places them in the right spot. currently needs to dl lua 5.3 and lunatic python from the available github.

--- setup.sh
+++ setup.sh
@@ -1,17 +1,16 @@
-cd ..
-mkdir src
+mkdir extSrc
 mkdir lunatexPython/bin
-cd src
+cd extSrc
 git clone https://github.com/bastibe/lunatic-python
 cd lunatic-python
 wget https://www.lua.org/ftp/lua-5.3.6.tar.gz
 tar -zvxf lua-5.3.6.tar.gz
 cd lua-5.3.6
-patch -p1 < ../../../setup/luaSOpatch.patch
+patch -p1 < ../../../patches/luaSOpatch.patch
 make linux
 make local
 cd ..
-patch -p1 < ../../setup/lunaticLocalLua.patch
+patch -p1 < ../../patches/lunaticLocalLua.patch
 cmake .
 make
 cp bin/python.so ../../lunatexPython/bin/

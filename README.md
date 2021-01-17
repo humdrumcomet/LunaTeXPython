f59542e036bb7b503d7b27fea4696a016f5ba8ae
Aaron English <humdrumcomet@protonmail.com>
2021-01-17 11:17:55 -0500

    Added removal function to setup script to cleanup the lua and lunatic-python files once they were no longer needed. Also adding a readme with installation instructions

---
+++ README.md
@@ -0,0 +1,8 @@
+# LunaTeX Python - The LuaTeX Python Bridge
+## Introduction
+This is a LuaLaTeX package that makes use of the 
+## Warnings
+This code is experimental, and has only been 
+## Installation
+## Basic Usage
+## To Do's
--- setup.sh
+++ setup.sh
@@ -1,17 +1,20 @@
-mkdir extSrc
-mkdir lunatexPython/bin
-cd extSrc
-git clone https://github.com/bastibe/lunatic-python
+mkdir extSrc                                        # Create the build directory for the shared
+mkdir lunatexPython/bin                             # Libraries
+cd extSrc                                           # Enter the build directory, and clone from
+git clone https://github.com/bastibe/lunatic-python # the lunatic-python github page
 cd lunatic-python
-wget https://www.lua.org/ftp/lua-5.3.6.tar.gz
-tar -zvxf lua-5.3.6.tar.gz
+wget https://www.lua.org/ftp/lua-5.3.6.tar.gz       # Download a version of lua that matches the
+tar -zvxf lua-5.3.6.tar.gz                          # the current luatex version
 cd lua-5.3.6
-patch -p1 < ../../../patches/luaSOpatch.patch
-make linux
-make local
+patch -p1 < ../../../patches/luaSOpatch.patch       # Patch the lua make files to build the shared
+make linux                                          # libraries then perform a local (as in no
+make local                                          # writing to system directories)
 cd ..
-patch -p1 < ../../patches/lunaticLocalLua.patch
-cmake .
+patch -p1 < ../../patches/lunaticLocalLua.patch     # Patch the lunatic-python cmake files to
+cmake .                                             # look for python 3.x and use the local lua
 make
-cp bin/python.so ../../lunatexPython/bin/
-cp bin/lua.so ../../lunatexPython/bin/
+cp bin/python.so ../../lunatexPython/bin/           # copy over the shared libraries to the installation
+cp bin/lua.so ../../lunatexPython/bin/              # folder
+cd ../..
+rm -rf extSrc                                       # clean up (delete) the downloaded lua and 
+                                                    # lunatic-python folders
